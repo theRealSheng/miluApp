1f304f17792dd11ab0099655ff1f28b0ed564747
theRealSheng <therealsheng@theRealShengs-MacBook-Pro.local>
2019-11-04 20:29:39 +0100

    first commit

---
+++ src/screens/MainScreen/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+class MainScreen extends Component {
+	render() {
+		return (
+			<View>
+
+			</View>
+		)
+	}
+}
+
+export default MainScreen;
